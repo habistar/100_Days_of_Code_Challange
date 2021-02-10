6a7233db69a231582c60e450f270913b333bcf64
Umme Habiba <habistar0@gmail.com>
2021-02-10 14:26:38 +0530

    start of day_02

---
+++ Day_02/exercise_01.py
@@ -0,0 +1,29 @@
+# String Indexing
+
+print("Hello"[0]) # "H"
+print("Hello"[4]) # "o"
+
+
+# String Addition
+
+print("123" + "456") # "123456"
+
+
+#Integer
+
+print(123 + 345) # "468"
+
+# We can use _ instead of commas For Example: 123,456,789 is 123_456_789
+
+print(123_456_789) # "12346789"
+
+
+#Float
+
+print(3.14159)
+
+
+#Boolean
+
+True
+False
---
+++ Day_02/exercise_02.py
@@ -0,0 +1,8 @@
+a = "111"
+b = "222"
+c = 333
+
+print(a + b) #111222
+print(a + c) #type error
+print(a + str(c) #111333
+print(int(a) + c) #444
